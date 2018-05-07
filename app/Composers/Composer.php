7afdd6ea56f10e056cdad7dff967350dcb040185
Amelia Ikeda <amelia@dorks.io>
2018-05-07 09:50:14 +0100

    Clean up the base Composer a little

--- app/Composers/Composer.php
+++ app/Composers/Composer.php
@@ -7,38 +7,33 @@ use Illuminate\View\View;
 abstract class Composer
 {
     /**
-     * @var string
-     */
-    protected const MODEL = 'App\Model';
-
-    /**
      * The default data key for this composer.
-     * 
+     *
      * @var string
      */
-    protected const DATA_KEY = 'data';
+    protected const KEY = 'data';
 
     /**
      * How long this key should be cached for.
-     * 
+     *
      * @var string
      */
     protected const DURATION = 1440;
 
     /**
      * Bind data into the view.
-     * 
+     *
      * @param \Illuminate\View\View $view
      * @return void
      */
     public function compose(View $view)
     {
-        $view->with(static::DATA_KEY, $this->data());
+        $view->with(static::KEY, $this->data());
     }
 
     /**
      * Get a list of models from this composer.
-     * 
+     *
      * @return \Illuminate\Database\Eloquent\Collection|\App\Model[]|\Illuminate\Database\Eloquent\Model[]
      */
     protected function data()
@@ -58,7 +53,7 @@ abstract class Composer
 
     /**
      * Get the cache key for this composer.
-     * 
+     *
      * @return string
      */
     protected function key()
@@ -68,7 +63,7 @@ abstract class Composer
 
     /**
      * Get models loaded from the database.
-     * 
+     *
      * @return \Illuminate\Database\Eloquent\Collection|\App\Model[]|\Illuminate\Database\Eloquent\Model[]
      */
     abstract protected function load();
