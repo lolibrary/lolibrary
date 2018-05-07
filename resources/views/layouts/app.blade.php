0ebb6207b9ac2cdd3b6ffbfc6b2192777333baf0
Amelia Ikeda <amelia@dorks.io>
2018-05-07 08:02:40 +0100

    Slightly tweak the base layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,8 +44,7 @@
 
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
