c8501fd71deca7ed3a764b44528b3c2d4a5fdb95
Amelia Ikeda <amelia@dorks.io>
2018-05-13 15:58:08 +0100

    Properly link the favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="{{ asset('assets/favicon.png') }}" type="image/png" sizes="any">
+    <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/png" sizes="any">
     <title>{{ $title or config('app.name', 'Lolibrary') }}</title>
 
     <!-- CSRF Token -->
