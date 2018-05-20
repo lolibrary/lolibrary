07921fd5a646f8fe53163d749382a03449d95d74
Amelia Ikeda <amelia@dorks.io>
2018-05-20 20:21:15 +0100

    Fix UserFactory when WithoutEvents is specified.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,9 +15,12 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
+        'id' => uuid4(),
         'name' => $faker->name,
+        'username' => $faker->unique()->userName,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+        'email_token' => null,
     ];
 });
