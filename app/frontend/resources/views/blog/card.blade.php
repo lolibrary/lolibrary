d07800492d96feda80c3a587b5f4b58299772c22
Amelia Ikeda <amelia@dorks.io>
2018-06-20 20:24:42 +0100

    Finish off the initial homepage layout!

--- app/frontend/resources/views/blog/card.blade.php
+++ app/frontend/resources/views/blog/card.blade.php
@@ -1,7 +1,7 @@
 <div class="card" style="margin-bottom: 10px">
     <img class="card-img-top"
          src="{{ $post->image ?? asset('images/backgrounds/pattern_dark_blog-cropped.png') }}"
-         alt="" style="max-height: 180px; width: 100%; background-color: #ffffff;">
+         alt="" style="min-height: 120px; max-height: 150px; width: 100%; background-color: #ffffff;">
     <div class="card-body">
         <h5 class="card-title">{{ $post->title }}</h5>
         <h6 class="card-subtitle mb-2 text-muted text-right">
@@ -14,7 +14,7 @@
             @endif
         </h6>
 
-        <p class="card-text">
+        <p class="card-text" style="min-height: 15rem; overflow-y: hidden">
             {{-- post preview is raw HTML. blog post permission is only given to admin or higher. --}}
             {!! $post->preview !!}
         </p>
--- app/frontend/resources/views/homepage.blade.php
+++ app/frontend/resources/views/homepage.blade.php
@@ -18,38 +18,44 @@
     {{-- todo: put brands in here with their images --}}
     {{-- todo: carousel these! (or scroll left/right) --}}
     <h2 class="mt-5">{{ __('Brands') }}</h2>
-    <div style="overflow-x: scroll; white-space: nowrap; overflow-y: hidden">
+    <div style="display: flex; flex-wrap: nowrap; -webkit-overflow-scrolling: touch; overflow-x: auto; overflow-y: hidden">
         @foreach ($brands as $brand)
-            <div class="p-2 d-inline-block" style="width: 10rem; white-space: normal">
-                <div class="card shadow-sm" style="height: 9rem;">
-                    <a href="{{ $brand->url }}" class="card-link">
-                        <div class="card-body text-center">
-                            <div class="mx-auto px-2 py-1">
-                                <img crossorigin="anonymous" src="{{ $brand->image->url }}"
-                                    onerror="this.src = '{{ asset('images/icon.svg') }}'" class="img-circle mw-100" alt="" style="height: 60px">
-                            </div>
-                            <p class="text-muted small py-2 align-text-bottom">{{ $brand->name }}</p>
+            <div class="card shadow-sm m-2" style="flex: 0 0 auto">
+                <a href="{{ $brand->url }}">
+                    <div class="card-body p-0 m-0" style="height: 150px; width: 150px;">
+                        <div class="d-flex justify-content-center align-items-center" style="height: 100px">
+                            <img src="{{ $brand->image->url }}"
+                                onerror="this.src = '{{ asset('images/icon.svg') }}'"
+                                class="p-3 mh-100 mw-100" alt=""
+                                data-original-url="{{ $brand->image->url }}">
                         </div>
-                    </a>
-                </div>
+                        <div style="height: 50px" class="text-center">
+                            <p class="text-muted small p-2">
+                                {{ $brand->name }}
+                            </p>
+                        </div>
+                    </div>
+                </a>
             </div>
         @endforeach
     </div>
 
     <h2 class="mt-5">{{ __('Categories') }}</h2>
-    <div style="overflow-x: scroll; white-space: nowrap; overflow-y: hidden">
+    <div style="display: flex; flex-wrap: nowrap; -webkit-overflow-scrolling: touch; overflow-x: auto; overflow-y: hidden">
         @foreach ($categories as $category)
-            <div class="p-2 d-inline-block" style="width: 10rem; white-space: normal">
-                <div class="card shadow-sm" style="height: 9rem;">
-                    <a href="{{ $category->url }}" class="card-link">
-                        <div class="card-body text-center">
-                            <div class="mx-auto px-2 py-1">
-                                <img src="{{ asset('images/icon.svg') }}" class="img-circle mw-100" alt="" style="height: 60px">
-                            </div>
-                            <p class="text-muted py-2 small align-text-bottom">{{ $category->name }}</p>
+            <div class="card shadow-sm m-2" style="flex: 0 0 auto">
+                <a href="{{ $category->url }}">
+                    <div class="card-body p-0 m-0" style="height: 150px; width: 150px;">
+                        <div class="d-flex justify-content-center align-items-center" style="height: 100px">
+                            <img src="{{ asset('images/icon.svg') }}" class="p-3 mh-100 mw-100" alt="">
                         </div>
-                    </a>
-                </div>
+                        <div style="height: 50px" class="text-center">
+                            <p class="text-muted small p-2">
+                                {{ $category->name }}
+                            </p>
+                        </div>
+                    </div>
+                </a>
             </div>
         @endforeach
     </div>
