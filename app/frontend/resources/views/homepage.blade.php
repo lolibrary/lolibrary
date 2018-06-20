cbd86b454868e7d03af779df44a5f05823b53965
Amelia Ikeda <amelia@dorks.io>
2018-06-20 16:24:25 +0100

    Add recent items to the homepage

--- app/frontend/resources/views/homepage.blade.php
+++ app/frontend/resources/views/homepage.blade.php
@@ -25,7 +25,8 @@
                     <a href="{{ $brand->url }}" class="card-link">
                         <div class="card-body text-center">
                             <div class="mx-auto px-2 py-1">
-                                <img src="{{ asset('images/icon.svg') }}" class="img-circle mw-100" alt="" style="height: 60px">
+                                <img crossorigin="anonymous" src="{{ $brand->image->url }}"
+                                    onerror="this.src = '{{ asset('images/icon.svg') }}'" class="img-circle mw-100" alt="" style="height: 60px">
                             </div>
                             <p class="text-muted small py-2 align-text-bottom">{{ $brand->name }}</p>
                         </div>
@@ -56,7 +57,7 @@
     <h2 class="mt-5">{{ __('Recent Items') }}</h2>
     <div style="overflow-x: scroll; white-space: nowrap; overflow-y: hidden">
         @foreach ($recent as $item)
-            <div class="p-2 d-inline-block" style="width: 10rem; white-space: normal">
+            <div class="p-2 d-inline-block" style="width: 16rem; white-space: normal">
                 @include('items.card', compact('item'))
             </div>
         @endforeach
--- app/frontend/resources/views/items/card.blade.php
+++ app/frontend/resources/views/items/card.blade.php
@@ -1,10 +1,43 @@
 <div class="card">
     <div class="card-body">
-        <p class="h5" style="white-space: nowrap; overflow-x: hidden">
+        <p class="mb-0" style="white-space: nowrap; overflow-x: hidden;">
             {{ $item->english_name }}
         </p>
-        <p class="h6" style="white-space: nowrap; overflow-x: hidden">
+        <p class="text-muted small" style="white-space: nowrap; overflow-x: hidden">
             {{ $item->foreign_name }}
         </p>
+
+        <div style="height: 7rem" class="text-center">
+            <img src="{{ $item->image->url }}" class="mw-100 mh-100 rounded">
+        </div>
     </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+            <div class="d-flex">
+                <div class="text-left flex-fill" style="white-space: nowrap; overflow-x: ellipsis">
+                    <p class="m-0 p-0 small text-muted">
+                        {{ __('Brand') }}
+                    </p>
+                    <p class="p-0 m-0 small">
+                        <a href="{{ $item->brand->url }}">
+                            {{ $item->brand->name }}
+                        </a>
+                    </p>
+                </div>
+                <div class="text-right flex-fill" style="white-space: nowrap; overflow-x: hidden">
+                    <p class="m-0 p-0 small text-muted">
+                        {{ __('Category') }}
+                    </p>
+                    <p class="p-0 m-0 small">
+                        <a href="{{ $item->category->url }}">
+                            {{ $item->category->name }}
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </li>
+    </ul>
+    <a class="card-body py-3 text-center" href="{{-- $item->url --}}">
+        View Item
+    </a>
 </div>
