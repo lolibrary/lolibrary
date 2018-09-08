70bf69773aa57fd2c270b939415074b2bf62d3ca
Amelia Ikeda <ameliaikeda@users.noreply.github.com>
2018-09-08 11:03:53 +0100

    Add a layout for the profile sections and add base views for the wishlist/closet

--- app/frontend/resources/views/profile/closet.blade.php
+++ app/frontend/resources/views/profile/closet.blade.php
@@ -1,31 +1,41 @@
-@extends('layouts.app')
+@extends('profile.layout')
 
-@section('content')
-<div class="container">
+@section('profile')
+@if ($items->count() > 0)
     <div class="row">
-        <div class="col-md-4">
-            <div class="text-center">
-                <img src="{{ default_asset() }}" alt="" style="max-height: 150px; max-width: 150px; margin-top: 10px; margin-bottom: 10px" class="img-thumbnail circle">
-            </div>
-            
-            <div class="list-group">
-                    <a href="{{ route('profile') }}" class="list-group-item list-group-item-action">
-                        <i class="fal fa-fw fa-user"></i>
-                        {{ __('Profile') }}
-                    </a>
-                    <a href="{{ route('wishlist') }}" class="list-group-item list-group-item-action">
-                        <i class="fal fa-fw fa-star"></i>
-                        {{ __('Wishlist') }}
-                    </a>
-                    <a href="{{ route('closet') }}" class="list-group-item list-group-item-action active">
-                        <i class="fal fa-fw fa-tags"></i>
-                        {{ __('Closet') }}
-                    </a>
+        @foreach ($items as $item)
+            @include('items.card', $item)
+        @endforeach
+    </div>
+
+    {{ $items->links() }}
+@else
+<div class="text-center mt-5">
+    <p class="h2">There are no items in your closet.</p>
+    <p class="lead">Why not <a href="{{ route('search') }}">search for some items to add</a>?</p>
+</div>
+<div class="row pt-5">
+    <div class="col-4">
+        <div class="card bg-light text-muted">
+            <div class="card-body shadow-sm d-flex justify-content-center align-items-center">
+                <i class="fal fa-plus-circle fa-5x"></i>
             </div>
         </div>
-        <div class="col-md-8">
-            
+    </div>
+    <div class="col-4">
+        <div class="card bg-light text-muted">
+            <div class="card-body shadow-sm d-flex justify-content-center align-items-center">
+                <i class="fal fa-plus-circle fa-5x"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-4">
+        <div class="card bg-light text-muted">
+            <div class="card-body shadow-sm d-flex justify-content-center align-items-center">
+                <i class="fal fa-plus-circle fa-5x"></i>
+            </div>
         </div>
     </div>
 </div>
+@endif
 @endsection
