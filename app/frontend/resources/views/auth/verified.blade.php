72e77dbebe82d640466ffc8b491944b03ab93f27
Charlotte Powning <capowning@gmail.com>
2018-10-06 13:24:12 -0400

    Anchor tag linking to homepage on email verification

--- app/frontend/resources/views/auth/verified.blade.php
+++ app/frontend/resources/views/auth/verified.blade.php
@@ -6,7 +6,7 @@
         <div class="col text-center">
             <img style="max-height: 300px" src="{{ asset('images/banners/banner01.png') }}" alt="">
             <h2>{{ __("Your account is now verified!") }}</h2>
-            <h3 class="text-muted">{{ __("You can close this browser tab or go to the homepage.") }}</h3>
+            <h3 class="text-muted">{{ __("You can close this browser tab or <a href="lolibrary.org">go to the homepage.</a>") }}</h3>
         </div>
     </div>
 </div>
