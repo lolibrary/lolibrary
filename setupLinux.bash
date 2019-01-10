b700783f93e1a65b945c3e7029e723ce30055ddf
Chun <cvlangelbeats@gmail.com>
2019-01-10 18:00:50 +0100

    Update setupLinux.bash

--- setupLinux.bash
+++ setupLinux.bash
@@ -11,75 +11,75 @@ function clone_lolibrary {
     sudo git clone https://github.com/lolibrary/lolibrary.git
     cp ./thisbashname $baseDirectory/lolibrary/
     cd lolibrary
- }
+}
 
 function configure_everything_for_lolibrary {
-	sudo echo "Please run this from lolibrary root folder where docker-compose.yml is located."
-	#Base directory
-	baseDirectory=$(pwd)
+    sudo echo "Please run this from lolibrary root folder where docker-compose.yml is located."
+    #Base directory
+    baseDirectory=$(pwd)
 
-	#Rename .env.example to .env
-	sudo set -e
+    #Rename .env.example to .env
+    sudo set -e
 
-	if [ ! -f .env ]; then
-		sudo echo "Copying .env.example to .env"
-		sudo cp .env.example .env
-	fi
+    if [ ! -f .env ]; then
+        sudo echo "Copying .env.example to .env"
+        sudo cp .env.example .env
+    fi
 
-	# first, trust our tls certificate
-	sudo echo "Adding a certificate to your trust store (pki/certificate.pem)"
-	sudo apt-get update
-	sudo apt-get install ca-certificates -y
-	sudo cp $baseDirectory/pki/certificate.pem --target-directory="/usr/local/share/ca-certificates/certificate.crt"
-	sudo mv /usr/local/share/ca-certificates/certificate.pem /usr/local/share/ca-certificates/certificate.crt
-	sudo update-ca-certificates
+    # first, trust our tls certificate
+    sudo echo "Adding a certificate to your trust store (pki/certificate.pem)"
+    sudo apt-get update
+    sudo apt-get install ca-certificates -y
+    sudo cp $baseDirectory/pki/certificate.pem --target-directory="/usr/local/share/ca-certificates/certificate.crt"
+    sudo mv /usr/local/share/ca-certificates/certificate.pem /usr/local/share/ca-certificates/certificate.crt
+    sudo update-ca-certificates
 
-	#Installing DNS
-	echo "Installing dnsmasq via apt-get"
-	sudo apt-get install dnsmasq -y
-	sudo service dnsmasq start
-	sudo cp /etc/host /etc/hosts_backup
-	sed -i "2i127.0.0.1  lolibrary.test lolibrary" /etc/hosts
+    #Installing DNS
+    echo "Installing dnsmasq via apt-get"
+    sudo apt-get install dnsmasq -y
+    sudo service dnsmasq start
+    sudo cp /etc/host /etc/hosts_backup
+    sed -i "2i127.0.0.1  lolibrary.test lolibrary" /etc/hosts
 
-	#Install Docker
-	sudo apt-get update
-	sudo apt-get install apt-transport-https curl gnupg2 software-properties-common -y
-	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add
-	sudo apt-key fingerprint 0EBFCD88
-	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-	sudo apt-get update
-	sudo apt-get install docker-ce -y
-	sudo docker run hello-world
+    #Install Docker
+    sudo apt-get update
+    sudo apt-get install apt-transport-https curl gnupg2 software-properties-common -y
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add
+    sudo apt-key fingerprint 0EBFCD88
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+    sudo apt-get update
+    sudo apt-get install docker-ce -y
+    sudo docker run hello-world
 
-	#Install Docker-compose
-	sudo curl -L "https://github.com/docker/compose/releases/download/1.23"
+    #Install Docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.23"
 }
 
 function start_lolibrary_containers{
    	#Run correctly please
    	sudo echo "Please run this from lolibrary root folder where docker-compose.yml is located."
    
-	#now, docker-compose up and create the initial volumes/files etc
-	sudo echo "Starting docker services..."
-	sudo docker-compose up -d
+    #now, docker-compose up and create the initial volumes/files etc
+    sudo echo "Starting docker services..."
+    sudo docker-compose up -d
 
-	# run install commands; can be run each time.
-	sudo echo "Installing next"
-	sudo docker-compose exec www.lolibrary.test composer install
+    # run install commands; can be run each time.
+    sudo echo "Installing next"
+    sudo docker-compose exec www.lolibrary.test composer install
 
-	sudo echo "Checking the database is up"
-	sudo docker-compose exec www.lolibrary.test php artisan wait:db --timeout=15 --sleep=200
+    sudo echo "Checking the database is up"
+    sudo docker-compose exec www.lolibrary.test php artisan wait:db --timeout=15 --sleep=200
 
-	status=$?
+    status=$?
 
-	if [ $status -eq 1 ]; then
-		sudo echo "Database timed out";
-		exit 1
-	fi
+    if [ $status -eq 1 ]; then
+        sudo echo "Database timed out";
+        exit 1
+    fi
 
-	sudo docker-compose exec www.lolibrary.test php artisan migrate --seed
+    sudo docker-compose exec www.lolibrary.test php artisan migrate --seed
 
-	sudo echo "All done - it may be a little while until the site comes up, because nodejs is actively building the frontend via Laravel Mix."
+    sudo echo "All done - it may be a little while until the site comes up, because nodejs is actively building the frontend via Laravel Mix."
 }
 
 #Bash Menu
