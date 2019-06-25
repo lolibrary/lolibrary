024cd206bcf8f736d7208e93bb40b1cdda92a182
Amelia Ikeda <amelia@lolibrary.org>
2019-06-26 00:32:47 +0100

    Add the scaffolding for the category service

---
+++ service.category/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/lolibrary/lolibrary/foundation"
+	"github.com/lolibrary/lolibrary/libraries/filters"
+
+	"github.com/lolibrary/lolibrary/service.category/dao"
+	"github.com/lolibrary/lolibrary/service.category/handler"
+)
+
+func main() {
+	svc := handler.Service().Filter(filters.CommonFilters)
+	srv := foundation.Init("service.category")
+	db := dao.Init()
+	defer db.Close()
+
+	srv.Run(svc)
+}
