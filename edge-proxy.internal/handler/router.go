ec1c90718b4b9ee827db32c1912df824ddeda008
Amelia Ikeda <amelia@lolibrary.org>
2019-06-25 13:45:25 +0100

    Rebuild the edge proxy to regexp match before forwarding

--- edge-proxy.internal/handler/router.go
+++ edge-proxy.internal/handler/router.go
@@ -1,11 +1,20 @@
 package handler
 
 import (
-	"strings"
+	"fmt"
+	"regexp"
 
 	"github.com/monzo/slog"
 	"github.com/monzo/terrors"
 	"github.com/monzo/typhon"
+)
+
+var (
+	reAPI     = regexp.MustCompile(`^/service.api.([a-z\-]+)/([a-z\-]*)$`)
+	reService = regexp.MustCompile(`^/service.([a-z\-]+)/([a-z\-]*)$`)
+
+	// requestFormat is the format we'll send to upstream services.
+	requestFormat = "http://%s/%s"
 )
 
 // Service returns a typhon service.
@@ -15,18 +24,45 @@ func Service() typhon.Service {
 
 // Proxy takes an incoming request and proxies it into the cluster.
 func Proxy(req typhon.Request) typhon.Response {
+	path := req.URL.Path
+
 	switch {
-	case strings.HasPrefix(req.URL.Path, "/service."):
+	case reAPI.MatchString(path):
+		return handleAPI(req)
+	case reService.MatchString(path):
 		return handleService(req)
 	}
 
-	return typhon.Response{Error: terrors.BadRequest("missing_endpoint", "Don't know how to query that service!", nil)}
+	return typhon.Response{Error: terrors.NotFound("missing_endpoint", "Don't know how to query that service!", nil)}
+}
+
+// handle takes a request and sends it to an internal service.
+func handle(req typhon.Request, service, path string) typhon.Response {
+	url := fmt.Sprintf(requestFormat, service, path)
+
+	// TODO: validate that service exists first via a TCP dial and return a 404, service %s not found.
+
+	slog.Trace(req, "Sending request to: %v", path)
+
+	return typhon.NewRequest(req, req.Method, url, req.Body).Send().Response()
+}
+
+// handleAPI takes a request and sends it to an internal API endpoint
+func handleAPI(req typhon.Request) typhon.Response {
+	parts := reAPI.FindStringSubmatch(req.URL.Path)
+	if len(parts) != 3 {
+		return typhon.Response{Error: terrors.NotFound("bad_endpoint", "Unable to determine endpoint.", nil)}
+	}
+
+	return handle(req, "/s-api-"+parts[1], parts[2])
 }
 
 // handleService takes a request and sends it to an internal service.
 func handleService(req typhon.Request) typhon.Response {
-	path := "http://s-" + strings.TrimPrefix(req.URL.Path, "/service.")
-	slog.Trace(req, "Sending request to: %v", path)
+	parts := reService.FindStringSubmatch(req.URL.Path)
+	if len(parts) != 3 {
+		return typhon.Response{Error: terrors.NotFound("bad_endpoint", "Unable to determine endpoint.", nil)}
+	}
 
-	return typhon.NewRequest(req, req.Method, path, req.Body).Send().Response()
+	return handle(req, "/s-"+parts[1], parts[2])
 }
