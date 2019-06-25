9174c8638f743dab240c769ad0f4e9b31e78079f
Amelia Ikeda <amelia@lolibrary.org>
2019-06-25 19:36:33 +0100

    Fix edge proxy routing

--- edge-proxy.internal/handler/router.go
+++ edge-proxy.internal/handler/router.go
@@ -26,6 +26,8 @@ func Service() typhon.Service {
 func Proxy(req typhon.Request) typhon.Response {
 	path := req.URL.Path
 
+	slog.Trace(req, "Incoming request to: %v", path)
+
 	switch {
 	case reAPI.MatchString(path):
 		return handleAPI(req)
@@ -42,7 +44,7 @@ func handle(req typhon.Request, service, path string) typhon.Response {
 
 	// TODO: validate that service exists first via a TCP dial and return a 404, service %s not found.
 
-	slog.Trace(req, "Sending request to: %v", path)
+	slog.Trace(req, "Sending request to: %v", url)
 
 	return typhon.NewRequest(req, req.Method, url, req.Body).Send().Response()
 }
@@ -54,7 +56,7 @@ func handleAPI(req typhon.Request) typhon.Response {
 		return typhon.Response{Error: terrors.NotFound("bad_endpoint", "Unable to determine endpoint.", nil)}
 	}
 
-	return handle(req, "/s-api-"+parts[1], parts[2])
+	return handle(req, "s-api-"+parts[1], parts[2])
 }
 
 // handleService takes a request and sends it to an internal service.
@@ -64,5 +66,5 @@ func handleService(req typhon.Request) typhon.Response {
 		return typhon.Response{Error: terrors.NotFound("bad_endpoint", "Unable to determine endpoint.", nil)}
 	}
 
-	return handle(req, "/s-"+parts[1], parts[2])
+	return handle(req, "s-"+parts[1], parts[2])
 }
