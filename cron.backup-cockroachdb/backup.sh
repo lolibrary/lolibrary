7f98046d276fc56416aafddf1cc786aa77557e14
Amelia Ikeda <52548998+amelia@users.noreply.github.com>
2019-07-06 13:11:14 +0100

    Add a backup CronJob for CockroachDB

---
+++ cron.backup-cockroachdb/backup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+rst="\033[0m"
+grn="\033[0;32m"
+
+database=$1
+bucket=$2
+serviceaccount=$3
+
+if [[ "$database" = "" ]]; then
+  echo "⚠️  Database not set."
+  echo -e " > Usage: backup <${grn}database${rst}> <${grn}gs://bucket${rst}> <${grn}service-account${rst}>"
+  exit 1
+fi
+
+if [[ "$bucket" = "" ]]; then
+  echo "⚠️  Bucket not set."
+  echo -e " > Usage: backup <${grn}database${rst}> <${grn}gs://bucket${rst}> <${grn}service-account${rst}>"
+  exit 1
+fi
+
+if [[ "$serviceaccount" = "" ]]; then
+  echo "⚠️  Service account not set."
+  echo -e " > Usage: backup <${grn}database${rst}> <${grn}gs://bucket${rst}> <${grn}service-account${rst}>"
+  exit 1
+fi
+
+current_date=$(date +%Y-%m-%dT%T%z)
+filename="${database}-${current_date}.sql"
+
+# activate our GCP service account
+echo -e "⬆️  Activating service account ${grn}${serviceaccount}${rst}"
+gcloud auth activate-service-account $GCP_SERVICE_ACCOUNT_NAME --key-file=/var/run/secrets/google/key.json
+echo "✅  Service account activated"
+
+# dump the file
+echo -e "💾 Dumping backup to ${grn}${filename}${rst}"
+cockroach dump $database > /tmp/$filename
+echo "✅  Backup created successfully"
+
+# upload to GCP
+echo -e "⌛️  Uploading backup to ${grn}${bucket}/${filename}${rst}"
+gsutil cp /tmp/$filename $bucket/$filename
+echo "✅  Uploaded successfully"
+
+rm -rf /tmp/$filename
+
+# ping healthcheck to say we uploaded properly.
+# curl ...
+echo "Not running cron healthcheck ping yet (not enabled)"
