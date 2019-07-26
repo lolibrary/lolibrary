6719153d9a0e5f78b6e8b4f3bc955289f931ac2e
Amelia Ikeda <52548998+amelia@users.noreply.github.com>
2019-07-26 09:35:58 +0100

    Make a new idgen.Flake func

--- libraries/idgen/idgen.go
+++ libraries/idgen/idgen.go
@@ -1,9 +1,15 @@
 package idgen
 
 import (
+	"context"
+	"fmt"
+
+	idproto "github.com/lolibrary/lolibrary/service.id/proto"
 	"github.com/monzo/terrors"
 	uuid "github.com/nu7hatch/gouuid"
 )
+
+var mocked = false
 
 // New creates a new UUID.
 func New() (string, error) {
@@ -23,4 +29,24 @@ func RandomTestID() string {
 	}
 
 	return id
+}
+
+// Flake grabs a generated flake ID from service.id.
+func Flake(ctx context.Context, prefix string) (string, error) {
+	if mocked {
+		return fmt.Sprintf("%s_%s", prefix, RandomTestID()), nil
+	}
+
+	rsp, err := idproto.POSTGenerateFlakeRequest{
+		Prefix: prefix,
+	}.Send(ctx).DecodeResponse()
+	if err != nil {
+		return "", err
+	}
+
+	return rsp.Id, nil
+}
+
+func Mock() {
+	mocked = true
 }
