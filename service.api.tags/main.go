dff43c5e9e5ed0900fd2e57f5a7717ec3fa06972
Amelia Ikeda <52548998+amelia@users.noreply.github.com>
2019-07-26 19:44:34 +0100

    Add filters to service.api.tags

--- service.api.tags/main.go
+++ service.api.tags/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"github.com/lolibrary/lolibrary/foundation"
+	"github.com/lolibrary/lolibrary/libraries/filters"
 	"github.com/lolibrary/lolibrary/service.api.tags/handler"
 )
 
 func main() {
 	srv := foundation.Init("service.api.tags")
-	svc := handler.Service()
+	svc := handler.Service().Filter(filters.CommonFilters)
 
 	srv.Run(svc)
 }
