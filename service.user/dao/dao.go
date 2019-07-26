8f00ddef1076aaca9f667831dbe045976f9eb66f
Amelia Ikeda <52548998+amelia@users.noreply.github.com>
2019-07-26 20:48:55 +0100

    Remove gorm from service.user

--- service.user/dao/dao.go
+++ service.user/dao/dao.go
@@ -3,19 +3,27 @@ package dao
 import (
 	"io"
 
-	"github.com/jinzhu/gorm"
+	"cloud.google.com/go/firestore"
 	"github.com/lolibrary/lolibrary/libraries/database"
 )
 
 var (
-	DB *gorm.DB
+	Firestore *firestore.Client
+
+	usersByID       *firestore.CollectionRef
+	usersByUsername *firestore.CollectionRef
+	usersByEmail    *firestore.CollectionRef
 )
 
 // Init starts up the database access object package and configures a database.
 func Init() io.Closer {
-	if DB == nil {
-		DB = database.Connect()
+	if Firestore == nil {
+		Firestore = database.NewClient()
 	}
 
-	return DB
+	usersByID = Firestore.Collection("users")
+	usersByUsername = Firestore.Collection("users-by-username")
+	usersByEmail = Firestore.Collection("users-by-email")
+
+	return Firestore
 }
