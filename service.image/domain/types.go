7cfd04b7e5f0ae1dcb0c2ed57e0ef011c654b68d
Amelia Ikeda <52548998+amelia@users.noreply.github.com>
2019-07-26 21:01:54 +0100

    Update protobufs

--- service.image/domain/types.go
+++ service.image/domain/types.go
@@ -1,1 +1,14 @@
 package domain
+
+import "time"
+
+type Image struct {
+	ID         string    `firestore:"id"`
+	ItemID     string    `firestore:"item_id"`
+	UploaderID string    `firestore:"uploader_id"`
+	Filename   string    `firestore:"filename"`
+	Type       string    `firestore:"type"`
+	Uploaded   bool      `firestore:"uploaded"`
+	CreatedAt  time.Time `firestore:"created_at"`
+	UpdatedAt  time.Time `firestore:"updated_at"`
+}
